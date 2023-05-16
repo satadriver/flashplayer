56467913ebf212e2a13ed568afd2f1a906da198b
satadriver <satadriver@sina.com>
2023-05-16 19:35:02 +0800

    commit

--- apkUnshell-new/app/.externalNativeBuild/cmake/release/arm64-v8a/cmake_install.cmake
+++ apkUnshell-new/app/.externalNativeBuild/cmake/release/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Android/project/apkUnshell/app
+# Install script for directory: D:/Android/project/flashplayer/apkUnshell-new/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Android/project/apkUnshell/app/.externalNativeBuild/cmake/release/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Android/project/flashplayer/apkUnshell-new/app/.externalNativeBuild/cmake/release/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
